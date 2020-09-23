14ed32f17c5c5fbef769be2b399aafa41a2f6604
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2020-09-24 01:14:39 +0500

    Published V1
    Naveed Links
    --------------------
    Naveed Links v1
    
    Ready to go online
    --------------------

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Naveed Links</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" href="css/iStyle.css">
+	</head>
+	<body>
+		<div class="container">
+				<?php include "html/header.html"; ?><!-- Header -->
+			<br/>
+			<div class="mainContent"> <!-- Main Content starts here -->
+				<?php include "html/slideShow.html"; ?> <!-- Slide Show -->
+				<br/><br/><br/>
+				<div class="blocks">
+					<h1>Why Naveed Links...?</h1>
+					<p>Quick and Affordable Smart Phone Services..!</p>
+					<br/>
+					<h3>Our aim is to offer outstanding services of repair works or troubleshoot your smart phones. For us, the repair work is not just about finding the fault and fixing it, it is about quality parts that are made to last long. We make sure that each of the parts used for repairing are brand new and of the highest quality.</h3>
+				</div>
+				<br/>
+				<div>
+					<h1>We offer best repair services at most affordable cost!</h1>
+					<div class="columns">
+						<h1>No Fix, No Fee</h1>
+						<p>We won't ask ever for upfront payment. You'll only pay once your device is fixed!</p>
+					</div>
+					<div class="columns">
+						<h1>100% Data Security</h1>
+						<p>Complete Data Security. We 100% Guarantee to Secure your Data.</p>
+					</div>
+					<div class="columns">
+						<h1>Experienced Staff</h1>
+						<p>Our technicians are Certified and carry years of smart phone repair experience.</p>
+					</div>
+				</div>
+				<br/><hr/>
+			</div> <!-- Main Content ends here -->
+			<br/>
+			<?php include "html/footer.html"; ?> <!-- Footer -->
+		</div>
+	</body>
+</html>
